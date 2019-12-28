0725f89d32c96563f959dd7177352e06c6939569
NaeNate <nate@qstreet.org>
2019-12-28 06:36:37 -0500

    E

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
   var todoApp = {
     todos: [],
     display: function () {
+      if (this.todos.length < 1) {
+        console.log("No todos today");
+      }
       console.log("My Todos: ", this.todos);
     },
     addNew: function (todoText) {
@@ -40,5 +43,34 @@
       this.display();
     }
   }
+
+/**
+ * Listeners and Event Handlers
+ */
+
+function clickHandler(event){
+  if (!event.target.matches('button')) return;
+  console.log(event);
+  if (event.target.matches('#btnDisplay')) {
+    todoApp.display();
+  }
+  if (event.target.matches('#btnToggle')) {
+    todoApp.toggleComplete();
+  }
+  if (event.target.matches('#btnAdd')) {
+    var inputValue = document.querySelector('#inputAdd').value;
+    todoApp.addNew(inputValue);
+    document.querySelector('#inputAdd').value = '';
+  }
+  if (event.target.matches('#btnChange')) {
+    var toBeChanged = document.querySelector('#inputToBeChanged').value;
+    var changeTo = document.querySelector('#inputChangeTo').value;
+    todoApp.change(toBeChanged, changeTo);
+    document.querySelector('#inputToBeChanged').value = '';
+    document.querySelector('#inputChangeTo').value = '';
+  }
+}
+
+document.addEventListener('input', clickHandler, false);
 
 })()
